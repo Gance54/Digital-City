392a1ea4c70748febaea48683ff670f69ddc3834
Mykhailo Sopiha <mykhailo.sopiha@gmail.com>
2024-08-26 21:06:32 -0400

    Initial commit
    Signed-off-by: Mykhailo Sopiha <mykhailo.sopiha@gmail.com>

---
+++ shared/src/commonMain/kotlin/com/digital/digitalcity/Greeting.kt
@@ -0,0 +1,9 @@
+package com.digital.digitalcity
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
